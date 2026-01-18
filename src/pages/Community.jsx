9dae538212fb05f551f172e0a2944975f8151ff5
rayan ouabri <rayan.ouabri1@gmail.com>
2026-01-18 16:01:28 +0100

    Fix: Remplacer React.Fragment par div wrapper dans Community.jsx pour eviter erreur esbuild

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -262,7 +262,7 @@ export default function Community() {
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         {!selectedPost ? (
-          <React.Fragment>
+          <div>
             {/* Filter */}
             <div className="mb-6">
               <Select value={categoryFilter} onValueChange={setCategoryFilter}>
@@ -404,7 +404,7 @@ export default function Community() {
                 </div>
               )}
             </div>
-          </React.Fragment>
+          </div>
         ) : (
           <div>
             {/* Thread View */}
